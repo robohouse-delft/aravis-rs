61db07b66d91e97195bc0e08a87e41480101a51b
Maarten de Vries <maarten@de-vri.es>
2019-10-26 17:10:15 +0200

    Generate crates.

---
+++ aravis/src/lib.rs
@@ -0,0 +1,19 @@
+extern crate aravis_sys;
+extern crate gio;
+extern crate gio_sys;
+#[macro_use]
+extern crate glib;
+extern crate glib_sys;
+extern crate gobject_sys;
+
+macro_rules! assert_initialized_main_thread {
+    () => {};
+}
+
+/// No-op.
+macro_rules! skip_assert_initialized {
+    () => {};
+}
+
+mod auto;
+pub use auto::*;
