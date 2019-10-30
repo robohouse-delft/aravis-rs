a594b74860a4fc30119bd9ba0aff115d68c9c0e9
Maarten de Vries <maarten@de-vri.es>
2019-10-30 22:56:33 +0100

    Add aravis::get_device_list().

--- aravis/src/lib.rs
+++ aravis/src/lib.rs
@@ -6,6 +6,8 @@ extern crate glib;
 extern crate glib_sys;
 extern crate gobject_sys;
 
+use std::ffi::CString;
+
 macro_rules! assert_initialized_main_thread {
     () => {};
 }
@@ -17,3 +19,27 @@ macro_rules! skip_assert_initialized {
 
 mod auto;
 pub use auto::*;
+
+#[derive(Debug)]
+pub struct DeviceInfo {
+    pub id: CString,
+    pub physical_id: CString,
+    pub vendor: CString,
+    pub model: CString,
+    pub protocol: CString,
+    pub address: CString,
+}
+
+pub unsafe fn get_device_list() -> Vec<DeviceInfo> {
+    aravis_sys::arv_update_device_list();
+    let count = aravis_sys::arv_get_n_devices();
+
+    (0..count).map(|i| DeviceInfo {
+        id:          std::ffi::CStr::from_ptr(aravis_sys::arv_get_device_id(i)).into(),
+        physical_id: std::ffi::CStr::from_ptr(aravis_sys::arv_get_device_physical_id(i)).into(),
+        vendor:      std::ffi::CStr::from_ptr(aravis_sys::arv_get_device_vendor(i)).into(),
+        model:       std::ffi::CStr::from_ptr(aravis_sys::arv_get_device_model(i)).into(),
+        protocol:    std::ffi::CStr::from_ptr(aravis_sys::arv_get_device_protocol(i)).into(),
+        address:     std::ffi::CStr::from_ptr(aravis_sys::arv_get_device_address(i)).into(),
+    }).collect()
+}
