bce08f9c1b061413681f6c2cf239bd91d443d30b
Maarten de Vries <maarten@de-vri.es>
2020-04-13 22:10:23 +0200

    Document manual/mod.rs and fix clippy warnings.

--- aravis/src/manual/mod.rs
+++ aravis/src/manual/mod.rs
@@ -26,14 +26,17 @@ pub type ArcImage<P> = image::ImageBuffer<P, std::sync::Arc<[u8]>>;
 
 static INITIALIZED: AtomicBool = AtomicBool::new(false);
 
+/// Token representing access to the global state of the Aravis library.
 pub struct Aravis {
 	_phantom: (),
 }
 
+/// Error returned when [`Aravis::initialize`] is called more than once.
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub struct AlreadyInitializedError;
 
-#[derive(Debug)]
+/// Information identifying a GenICam camera.
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct DeviceInfo {
 	pub id: CString,
 	pub physical_id: CString,
@@ -44,6 +47,11 @@ pub struct DeviceInfo {
 }
 
 impl Aravis {
+	/// Retrieve a token to access global state of the Aravis library.
+	///
+	/// This may only be called once.
+	/// Any subsequent invocation after the first will return a [`AlreadyInitializedError`].
+	#[allow(clippy::bool_comparison)]
 	pub fn initialize() -> Result<Aravis, AlreadyInitializedError> {
 		if INITIALIZED.swap(true, Ordering::AcqRel) == true {
 			Err(AlreadyInitializedError)
@@ -52,11 +60,19 @@ impl Aravis {
 		}
 	}
 
+	/// Enumerate all available GenICam devices.
 	pub fn get_device_list(&self) -> Vec<DeviceInfo> {
 		unsafe { get_device_list() }
 	}
 }
 
+/// Enumerate all available GenICam devices without mutual exclusion.
+///
+/// # Safety
+/// This function is unsafe because it accesses global state of the Aravis library,
+/// without guarantee that this thread is the only one accessing it.
+///
+/// See [`Aravis::get_device_list`] for a safe alternative.
 pub unsafe fn get_device_list() -> Vec<DeviceInfo> {
 	aravis_sys::arv_update_device_list();
 	let count = aravis_sys::arv_get_n_devices();
